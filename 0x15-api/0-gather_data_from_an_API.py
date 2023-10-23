47c268fd15da31c2d613019f37d05f3651e04f68
Benalla-Aiman <benallaaiman@gmailcom>
2023-10-23 13:49:27 -0700

    this is the task 00 of API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,46 +1,25 @@
+#!/usr/bin/python3
+"""Script that uses REST API"""
 import requests
 import sys
 
-def get_todo_list_progress(employee_id):
-    # Base URLs
-    user_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}"
-    todos_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}/todos"
-
-    # Getting user data
-    user_response = requests.get(user_url)
-    if user_response.status_code != 200:
-        print(f"Failed to fetch data for user ID {employee_id}.")
-        return
-
-    user_data = user_response.json()
-    employee_name = user_data["name"]
-
-    # Getting todos data
-    todos_response = requests.get(todos_url)
-    if todos_response.status_code != 200:
-        print(f"Failed to fetch todo data for user ID {employee_id}.")
-        return
-
-    todos_data = todos_response.json()
-    total_tasks = len(todos_data)
-    done_tasks = [task["title"] for task in todos_data if task["completed"]]
-    
-    # Printing the output
-    print(f"Employee {employee_name} is done with tasks({len(done_tasks)}/{total_tasks}):")
-    for task in done_tasks:
-        print(f"\t {task}")
-
-
 if __name__ == "__main__":
-    if len(sys.argv) < 2:
-        print("Please provide an employee ID as an argument.")
-        sys.exit(1)
+    if len(sys.argv) == 2 and sys.argv[1].isdigit():
+        args = {"id": sys.argv[1]}
+        users = requests.get("https://jsonplaceholder.typicode.com/users",
+                             params=args).json()
+        args = {"userId": sys.argv[1]}
+        todos = requests.get("https://jsonplaceholder.typicode.com/todos",
+                             params=args).json()
+        todos_len = 0
+        todos_arr = []
+        for i in todos:
+            if i.get("completed"):
+                todos_arr.append(i)
+                todos_len += 1
 
-    try:
-        employee_id = int(sys.argv[1])
-    except ValueError:
-        print("Please provide a valid integer for employee ID.")
-        sys.exit(1)
+        print("Employee {} is done with tasks({}/{}):".format(
+              users[0].get("name"), todos_len, len(todos)))
 
-    get_todo_list_progress(employee_id)
-
+        for i in todos_arr:
+            print("\t {}".format(i.get("title")))
