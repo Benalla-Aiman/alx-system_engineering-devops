686d2a8b92eda0431d6cc1d0d5fb7ed5841682e1
Benalla-Aiman <benallaaiman@gmailcom>
2023-10-23 14:01:23 -0700

    this is the task 00 of API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,25 +1,47 @@
-#!/usr/bin/python3
-"""Script that uses REST API"""
 import requests
 import sys
 
+def fetch_employee_todo_progress(employee_id):
+    # Define URLs for employee and their todos
+    employee_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}"
+    todos_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}/todos"
+
+    # Fetch employee details and their todos
+    employee_response = requests.get(employee_url)
+    todos_response = requests.get(todos_url)
+
+    # Ensure both requests were successful
+    if employee_response.status_code != 200 or todos_response.status_code != 200:
+        print("Error fetching data from the API")
+        sys.exit(1)
+
+    # Parse the responses
+    employee = employee_response.json()
+    todos = todos_response.json()
+
+    # Filter completed tasks
+    completed_todos = [todo for todo in todos if todo['completed']]
+
+    # Print the results
+    print(f"Employee {employee['name']} is done with tasks({len(completed_todos)}/{len(todos)}):")
+    for todo in completed_todos:
+        print(f"\t {todo['title']}")
+
 if __name__ == "__main__":
-    if len(sys.argv) == 2 and sys.argv[1].isdigit():
-        args = {"id": sys.argv[1]}
-        users = requests.get("https://jsonplaceholder.typicode.com/users",
-                             params=args).json()
-        args = {"userId": sys.argv[1]}
-        todos = requests.get("https://jsonplaceholder.typicode.com/todos",
-                             params=args).json()
-        todos_len = 0
-        todos_arr = []
-        for i in todos:
-            if i.get("completed"):
-                todos_arr.append(i)
-                todos_len += 1
+    # Check for command line arguments
+    if len(sys.argv) < 2:
+        print("Usage: python3 script_name.py [employee_id]")
+        sys.exit(1)
 
-        print("Employee {} is done with tasks({}/{}):".format(
-              users[0].get("name"), todos_len, len(todos)))
+    # Get employee ID from the command line argument
+    employee_id = sys.argv[1]
+    
+    try:
+        employee_id = int(employee_id)
+    except ValueError:
+        print("Please provide a valid integer as the employee ID.")
+        sys.exit(1)
 
-        for i in todos_arr:
-            print("\t {}".format(i.get("title")))
+    # Fetch and display the progress
+    fetch_employee_todo_progress(employee_id)
+
