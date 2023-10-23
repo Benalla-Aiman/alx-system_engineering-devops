9ca08059a19dcf1d21f61b3885566580a108e952
Benalla-Aiman <benallaaiman@gmailcom>
2023-10-23 14:17:43 -0700

    this is the task 00 of 0x15

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,46 +1,23 @@
-#!/usr/bin/python3
-"""
-Script that uses the JSONPlaceholder API to display an employee's TODO list progress.
-"""
-
 import requests
 import sys
 
-
-def display_progress(employee_id):
-    """
-    Displays the TODO list progress of an employee from the JSONPlaceholder API.
-    """
-    # Base API endpoints
-    user_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}"
-    todos_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}/todos"
-
-    # Fetch data from API
-    user_response = requests.get(user_url)
-    todos_response = requests.get(todos_url)
-
-    # Check if user exists
-    if user_response.status_code != 200:
-        return
-
-    user_data = user_response.json()
-    employee_name = user_data.get("name")
-
-    todos_data = todos_response.json()
-    completed_tasks = [task for task in todos_data if task.get("completed")]
-    total_tasks = len(todos_data)
-
-    # Display the results
-    print(f"Employee {employee_name} is done with tasks({len(completed_tasks)}/{total_tasks}):")
-    for task in completed_tasks:
-        print("\t {}".format(task.get("title")))
-
-
 if __name__ == "__main__":
-    if len(sys.argv) > 1:
-        try:
-            employee_id = int(sys.argv[1])
-            display_progress(employee_id)
-        except ValueError:
-            pass
+    if len(sys.argv) < 2:
+        print('Usage: {} <employee_id>'.format(sys.argv[0]))
+        sys.exit(1)
 
+    employee_id = sys.argv[1]
+    user_url = "https://jsonplaceholder.typicode.com/users/{}".format(employee_id)
+    todos_url = "https://jsonplaceholder.typicode.com/users/{}/todos".format(employee_id)
+
+    response = requests.get(user_url)
+    user = response.json()
+    response = requests.get(todos_url)
+    todos = response.json()
+
+    done_tasks = [task for task in todos if task.get('completed') is True]
+    total_tasks = len(todos)
+
+    print("Employee {} is done with tasks({}/{}):".format(user['name'], len(done_tasks), total_tasks))
+    for task in done_tasks:
+        print("\t {}".format(task['title']))
