e12864501fa8264216ce73f38f92686b28f719d6
Benalla-Aiman <benallaaiman@gmailcom>
2023-10-23 13:48:06 -0700

    this is the task 00 of API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,14 +1,46 @@
-#!/usr/bin/python3
-"""Returns to-do list information for a given employee ID."""
 import requests
 import sys
 
-if __name__ == "__main__":
-    url = "https://jsonplaceholder.typicode.com/"
-    user = requests.get(url + "users/{}".format(sys.argv[1])).json()
-    todos = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
+def get_todo_list_progress(employee_id):
+    # Base URLs
+    user_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}"
+    todos_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}/todos"
 
-    completed = [t.get("title") for t in todos if t.get("completed") is True]
-    print("Employee {} is done with tasks({}/{}):".format(
-        user.get("name"), len(completed), len(todos)))
-    [print("\t {}".format(c)) for c in completed]
+    # Getting user data
+    user_response = requests.get(user_url)
+    if user_response.status_code != 200:
+        print(f"Failed to fetch data for user ID {employee_id}.")
+        return
+
+    user_data = user_response.json()
+    employee_name = user_data["name"]
+
+    # Getting todos data
+    todos_response = requests.get(todos_url)
+    if todos_response.status_code != 200:
+        print(f"Failed to fetch todo data for user ID {employee_id}.")
+        return
+
+    todos_data = todos_response.json()
+    total_tasks = len(todos_data)
+    done_tasks = [task["title"] for task in todos_data if task["completed"]]
+    
+    # Printing the output
+    print(f"Employee {employee_name} is done with tasks({len(done_tasks)}/{total_tasks}):")
+    for task in done_tasks:
+        print(f"\t {task}")
+
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("Please provide an employee ID as an argument.")
+        sys.exit(1)
+
+    try:
+        employee_id = int(sys.argv[1])
+    except ValueError:
+        print("Please provide a valid integer for employee ID.")
+        sys.exit(1)
+
+    get_todo_list_progress(employee_id)
+
