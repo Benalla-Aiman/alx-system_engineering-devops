65aa81421c856ea8deefa2c669fc831721e9b57d
Benalla-Aiman <benallaaiman@gmailcom>
2023-10-23 14:21:29 -0700

    this is the task 00 of 0x15

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,46 +1,14 @@
 #!/usr/bin/python3
-"""
-Script that uses the JSONPlaceholder API to display an employee's TODO list progress.
-"""
-
+"""Returns to-do list information for a given employee ID."""
 import requests
 import sys
 
-
-def display_progress(employee_id):
-    """
-    Displays the TODO list progress of an employee from the JSONPlaceholder API.
-    """
-    # Base API endpoints
-    user_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}"
-    todos_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}/todos"
-
-    # Fetch data from API
-    user_response = requests.get(user_url)
-    todos_response = requests.get(todos_url)
-
-    # Check if user exists
-    if user_response.status_code != 200:
-        return
-
-    user_data = user_response.json()
-    employee_name = user_data.get("name")
-
-    todos_data = todos_response.json()
-    completed_tasks = [task for task in todos_data if task.get("completed")]
-    total_tasks = len(todos_data)
-
-    # Display the results
-    print(f"Employee {employee_name} is done with tasks({len(completed_tasks)}/{total_tasks}):")
-    for task in completed_tasks:
-        print("\t {}".format(task.get("title")))
-
-
 if __name__ == "__main__":
-    if len(sys.argv) > 1:
-        try:
-            employee_id = int(sys.argv[1])
-            display_progress(employee_id)
-        except ValueError:
-            pass
+    url = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(url + "users/{}".format(sys.argv[1])).json()
+    todos = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
 
+    completed = [t.get("title") for t in todos if t.get("completed") is True]
+    print("Employee {} is done with tasks({}/{}):".format(
+        user.get("name"), len(completed), len(todos)))
+    [print("\t {}".format(c)) for c in completed]
