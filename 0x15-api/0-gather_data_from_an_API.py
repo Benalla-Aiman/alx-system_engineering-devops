d5d259d22da26ccc9d7bf0ce2e778e21fbd316f5
Benalla-Aiman <benallaaiman@gmailcom>
2023-10-23 14:15:31 -0700

    this is the task 00 of 0x15

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,21 +1,46 @@
 #!/usr/bin/python3
 """
-    Uses the fake API to get an employer
+Script that uses the JSONPlaceholder API to display an employee's TODO list progress.
 """
+
 import requests
-from sys import argv
+import sys
+
+
+def display_progress(employee_id):
+    """
+    Displays the TODO list progress of an employee from the JSONPlaceholder API.
+    """
+    # Base API endpoints
+    user_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}"
+    todos_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}/todos"
+
+    # Fetch data from API
+    user_response = requests.get(user_url)
+    todos_response = requests.get(todos_url)
+
+    # Check if user exists
+    if user_response.status_code != 200:
+        return
+
+    user_data = user_response.json()
+    employee_name = user_data.get("name")
+
+    todos_data = todos_response.json()
+    completed_tasks = [task for task in todos_data if task.get("completed")]
+    total_tasks = len(todos_data)
+
+    # Display the results
+    print(f"Employee {employee_name} is done with tasks({len(completed_tasks)}/{total_tasks}):")
+    for task in completed_tasks:
+        print("\t {}".format(task.get("title")))
+
 
 if __name__ == "__main__":
-    id_em = argv[1]
-    url_employ = "https://jsonplaceholder.typicode.com/users/{}".format(id_em)
-    url_todos = url_employ + "/todos"
-    r_employ = requests.get(url_employ).json()
-    r_todos = requests.get(url_todos).json()
-    name = r_employ.get("name")
-    total_num_task = r_todos
-    done_task = [task for task in r_todos if task.get("completed")]
-    output = "Employee {} is done with tasks({}/{}):".format(
-                name, len(done_task), len(total_num_task))
-    for task in done_task:
-        output += "\n\t " + task.get("title")
-    print(output)
+    if len(sys.argv) > 1:
+        try:
+            employee_id = int(sys.argv[1])
+            display_progress(employee_id)
+        except ValueError:
+            pass
+
